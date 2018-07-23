5569436f2a9767bb573c1fbc1a2e2ccfc5986394
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-07-23 16:38:33 +0300

    maven

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+/**
+ * created by s.a.miroshnychenko 7/23/2018
+ */
+
+public class MyTest {
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+
+    @Test
+    public void test3() {
+
+    }
+}
