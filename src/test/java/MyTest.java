b5f96000a15b1ac23810ef92eed410b1edfa4793
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-07-26 14:06:02 +0300

    maven

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,4 +1,6 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * created by s.a.miroshnychenko 7/23/2018
@@ -7,16 +9,14 @@ import org.junit.Test;
 public class MyTest {
     @Test
     public void test1() {
+        System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://mvnrepository.com");
+        Assert.assertEquals("https://mvnrepository.com/", driver.getCurrentUrl());
+        driver.quit();
 
     }
 
-    @Test
-    public void test2() {
 
-    }
-
-    @Test
-    public void test3() {
-
-    }
 }
